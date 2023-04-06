a78b44774bb1f91827768fd0f8d3bd69480ee8aa
KokoDoko <benni_jolink@hotmail.com>
2023-04-06 12:15:48 +0200

    add fish animation

--- src/js/resources.js
+++
@@ -1,9 +0,0 @@
-import { ImageSource, Sound, Resource, Loader } from 'excalibur'
-import fishImage from '../images/fish.png'
-
-const Resources = {
-    Fish: new ImageSource(fishImage)
-}
-const ResourceLoader = new Loader([Resources.Fish])
-
-export { Resources, ResourceLoader }
